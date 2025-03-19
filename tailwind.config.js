34d092dcbaa3ad8466b31dfb4cb423ddcc6fef37
arnavsharma <arnavsh2004@gmail.com>
2025-03-19 14:40:09 +0530

    :tada: Update home page
    code the first appearance of the app and add tailwind classes for designing and font selection

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "dark-green": "#104A43",
+        "light-green": "#83D3A7",
+        "footer-green": "#0E5441",
+      },
+    },
+  },
+};
